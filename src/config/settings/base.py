1f757324d160b007d28fd5154d2ce535088fa217
Sanjar1996 <xabibovsanjarbek@gmail.com>
2021-09-28 13:11:49 +0500

    base.html changed

--- src/config/settings/base.py
+++ src/config/settings/base.py
@@ -32,7 +32,7 @@ SECRET_KEY = "asdjalhfiuehrauiewlhrioqwejraoiejaiorjeoirwq3243"
 # DEBUG = env.bool("DEBUG", default=False)
 DEBUG = True
 
-ALLOWED_HOSTS = ["buxoro-tajriba.uz", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["toshkent-urmon.uz", "localhost", "127.0.0.1"]
 
 # Application definition
 
